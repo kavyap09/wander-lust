a1c48bb947cd21fe87d26dc01ab1141b1a19b42e
Kavya <pendyalakavya@gmail.com>
2024-05-14 16:42:03 +0530

    updated 2

--- schema.js
+++ schema.js
@@ -14,7 +14,7 @@ module.exports.listingSchema = joi.object({
 module.exports.reviewSchema=joi.object({
     review:joi.object({
         rating:joi.number().required().min(1).max(5),
-        comments:joi.string().required()
+        comment:joi.string().required()
     }).required()
 })
 
